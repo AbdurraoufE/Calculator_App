1da98982cd3d7b501e5c9ff6d24a7111baa7e9f0
Abdurraouf Etagiuri <Roofa.Etagiuri1@gmail.com>
2023-08-04 01:53:26 -0600

    added the delete and the clear buttons inside the frame

--- src/Calculator.java
+++ src/Calculator.java
@@ -28,10 +28,59 @@ public class Calculator implements ActionListener{
         // allows us to close the GUI
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         // x-y cords for GUI size
-        frame.setSize(420, 550);
+        frame.setSize(400, 550);
         frame.setLayout(null);
 
+        // information for the textfield of the program
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(font);
+        // does not allow to write in the textbox that displays numbers
+        textField.setEditable(false);
 
+        // buttons (8)
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        delButton = new JButton("DELETE");
+        equButton = new JButton("=");
+        clrButton = new JButton("CLEAR");
+
+        //setting the buttons in an array funcButton uses JButton
+        funcButtons[0] = addButton;
+        funcButtons[1] = subButton;
+        funcButtons[2] = mulButton;
+        funcButtons[3] = divButton;
+        funcButtons[4] = decButton;
+        funcButtons[5] = delButton;
+        funcButtons[6] = equButton;
+        funcButtons[7] = clrButton;
+
+        // this loops through all the arrays of buttons of functions
+        for (int i =0; i<8; i++){
+            funcButtons[i].addActionListener(this);
+            funcButtons[i].setFont(font);
+            funcButtons[i].setFocusable(false);
+        }
+
+        // this loops through the number buttons (1 to 10)
+        for (int i =0; i<10; i++){
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(font);
+            numberButtons[i].setFocusable(false);
+        }
+
+        // location of buttons
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(205, 430, 145, 50);
+        // adding the buttons above to the frame:
+        frame.add(delButton);
+        frame.add(clrButton);
+
+        frame.add(textField);
         frame.setVisible(true);
     }
     public static void main(String[] args){
